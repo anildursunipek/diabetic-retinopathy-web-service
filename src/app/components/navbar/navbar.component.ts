196327cd97a9bb581a6e781e133d1ac9d80d1320
Dursun <93613110+Dursunipek@users.noreply.github.com>
2024-05-25 17:55:26 +0300

    Refactor navbar and upload-image components

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  // downloadImage(){
+  //   const fileUrl = 'src/assets/dr-sample-fundus-images.rar';
+  //   const fileName = 'dr-sample-fundus-images.rar';
+  //   this.downloadFile(fileUrl, fileName);
+  // }
 
+  // downloadFile(url: string, fileName: string) {
+  //   fetch(url, {
+  //     method: 'GET',
+  //     headers: {}
+  //   })
+  //   .then(response => response.blob())
+  //   .then(blob => {
+  //     const url = window.URL.createObjectURL(blob);
+  //     const a = document.createElement('a');
+  //     a.href = url;
+  //     a.download = fileName;
+  //     document.body.appendChild(a);
+  //     a.click();
+  //     a.remove();
+  //   })
+  //   .catch(error => console.error('Error downloading the file:', error));
+  // }
 }
